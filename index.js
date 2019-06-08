d823725d2d3f626c96f5487ad78e8dd81338c1da
tetsuuu <tetsu.kinosita@gmail.com>
2019-06-08 17:03:23 +0900

    fix index

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
+'use strict';
 const line = require('@line/bot-sdk');
+const crypto = require('crypto');
 const client = new line.Client({channelAccessToken: process.env.ACCESS_TOKEN});
-
-exports.handler = async function (event, context) {
-  let body = JSON.parse(event.body);
+ 
+exports.handler = function (event, context) {
+ let signature = crypto.createHmac('sha256', process.env.CHANNEL_SECRET).update(event.body).digest('base64');
+ let checkHeader = (event.headers || {})['X-Line-Signature'];
+ let body = JSON.parse(event.body);
+ if (signature === checkHeader) {
   if (body.events[0].replyToken === '00000000000000000000000000000000') {
-    let lambdaResponse = {
-      statusCode: 200,
-      headers: { "X-Line-Status" : "OK"},
-      body: '{"result":"connect check"}'
-    };
-    context.succeed(lambdaResponse);
-  } else {
-   var ret = {
-     'type': 'text',
-     'text': 'Hello World!'
-   };
-   var response = await client.replyMessage(body.events[0].replyToken, ret);
    let lambdaResponse = {
+    statusCode: 200,
+    headers: { "X-Line-Status" : "OK"},
+    body: '{"result":"connect check"}'
+   };
+   context.succeed(lambdaResponse);
+  } else {
+   let text = body.events[0].message.text;
+   const message = {
+    'type': 'text',
+    'text': text
+   };
+   client.replyMessage(body.events[0].replyToken, message)
+   .then((response) => { 
+    let lambdaResponse = {
      statusCode: 200,
      headers: { "X-Line-Status" : "OK"},
      body: '{"result":"completed"}'
-   };
-   context.succeed(lambdaResponse);
+    };
+    context.succeed(lambdaResponse);
+   }).catch((err) => console.log(err));
+  }
+ }else{
+  console.log('Signature authentication Error');
  }
 };
